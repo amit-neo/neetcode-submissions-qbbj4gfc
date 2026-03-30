6de9ae7e31e72b7622274d0ad7a31d08cc685b44
Amit Kumar <69421992+amit-neo@users.noreply.github.com>
2026-03-30 14:57:31 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let newStr = '';
+        for (let c of s){
+           if (this.isAlphanumeric(c)) {
+            newStr += c.toLowerCase();
+           } 
+        }
+        return newStr === newStr.split('').reverse().join('');
+    }
+
+    isAlphanumeric(char){
+        return (
+            (char >= 'a' && char <= 'z') ||
+            (char >= 'A' && char <= 'Z') ||
+            (char >= '0' && char <= '9')
+        );
+    }
+}
